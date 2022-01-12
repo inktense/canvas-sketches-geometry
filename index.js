d4952a4af273fda2e16a5c369a1b67314789b034
Bianca Popescu <bianca.popescu@biancas-mbp.home>
2022-01-12 18:19:54 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import canvasSketch from "canvas-sketch"
+
+const settings = {
+  dimensions: [ 1080, 1080 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+  };
+};
+
+canvasSketch(sketch, settings);
